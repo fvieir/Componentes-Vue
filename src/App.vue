603463d738200957161a2273c0cc7e3bb779c728
fvieir <fcvieira2@gmail.com>
2021-08-30 21:04:40 -0300

    Treinando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <v-navigation-drawer/>
+
+    <v-app-bar/>
+
+    <v-carousel/>
+
+    <Footer/>
+    
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from '@/components/templates/Rodape.vue'
+import Menu from '@/components/templates/Menu.vue'
+import NavBar from '@/components/templates/NavBar.vue'
+import Carousel from '@/components/widgets/Carousel.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Footer,
+    vNavigationDrawer: Menu,
+    'v-app-bar': NavBar,
+    vCarousel: Carousel,
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
